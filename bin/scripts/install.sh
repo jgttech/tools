b6da0b3c1eb1322473680f15fadd284ad64ad402
Jonathon Tech <jgt.tech@protonmail.com>
2024-08-15 23:58:47 -0500

    WIP 2024-08-15 23:58:46

--- bin/scripts/install.sh
+++ bin/scripts/install.sh
@@ -3,8 +3,13 @@ BASE_DIR=".scripts"
 BASE_PATH="${HOME}/${BASE_DIR}"
 CONF_FILE="pkg.sh"
 
+# Load the configuration into the installation.
 source "${BASE_PATH}/${CONF_FILE}"
 
-go build -o "${BASE_PATH}/bin/${VERSION}/tools" "${BASE_PATH}/main.go"
+# Build the tools.
+go build -o "${BASE_PATH}/bin/versions/${VERSION}/tools" "${BASE_PATH}/main.go"
+
+# Make the symbolic link to the current installation version.
+ln -s "${BASE_PATH}/bin/versions/${VERSION}/tools" "${BASE_PATH}/bin/local/tools"
 
 echo "Done"
