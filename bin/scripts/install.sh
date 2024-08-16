975eb879d0bd92d6849a2931c882ba0eba0e9908
Jonathon Tech <jgt.tech@protonmail.com>
2024-08-16 00:05:23 -0500

    WIP 2024-08-16 00:05:22

--- bin/scripts/install.sh
+++ bin/scripts/install.sh
@@ -3,11 +3,11 @@ BASE_DIR=".tools"
 BASE_PATH="${HOME}/${BASE_DIR}"
 CONF_PATH="${BASE_PATH}/pkg.sh"
 
-# Load the configuration into the installation.
-source ${CONF_PATH}
-
 # Clone the repo using the GitHub CLI.
 gh repo clone jgttech/tools ${BASE_PATH}
+
+# Load the configuration into the installation.
+source ${CONF_PATH}
 
 # Build the tools.
 go build -o "${BASE_PATH}/bin/versions/${VERSION}/tools" "${BASE_PATH}/main.go"
