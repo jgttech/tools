a308f3989b10455687b0435cc6610df55bada2c1
kronos <kronos@kronoss-MBP.router.asus.com>
2024-08-18 01:10:44 -0500

    "WIP"

--- cmds/edit/main.go
+++ cmds/edit/main.go
@@ -2,6 +2,7 @@ package edit
 
 import (
 	"context"
+	"fmt"
 	"jgttech/tools/path"
 	"jgttech/tools/sys"
 	"log"
@@ -9,14 +10,28 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+func editPath(cmd string) string {
+	var arg string
+
+	if cmd == "" {
+		arg = "."
+	} else {
+		arg = fmt.Sprintf("cmds/%s/main.go", cmd)
+	}
+
+	return fmt.Sprintf("nvim %s", arg)
+}
+
 func Command() *cli.Command {
 	return &cli.Command{
 		Name: "edit",
 		Action: func(ctx context.Context, cmd *cli.Command) error {
+			editCommand := cmd.Args().First()
+
 			baseDir, err := path.Join()
 			sys.Panic(err)
 
-			command := sys.StdCmd("nvim .")
+			command := sys.StdCmd(editPath(editCommand))
 			command.Dir = baseDir
 
 			if err := command.Run(); err != nil {
--- cmds/update/main.go
+++ cmds/update/main.go
@@ -2,6 +2,7 @@ package update
 
 import (
 	"context"
+	"fmt"
 
 	"jgttech/tools/path"
 	"jgttech/tools/sys"
@@ -12,26 +13,28 @@ import (
 func Command() *cli.Command {
 	return &cli.Command{
 		Name: "update",
-		Action: func(ctx context.Context, cmd *cli.Command) error {
+		Action: func(ctx context.Context, _ *cli.Command) error {
 			pwd, err := path.Join()
 			sys.Panic(err)
 
 			add := sys.StdCmd("git add .")
 			add.Dir = pwd
 
-			// commit := sys.StdCmd(fmt.Sprintf("git commit -m \"WIP %s\"", time.Now()))
-			// commit.Dir = pwd
+			err = add.Run()
+			sys.Panic(err)
+
+			// timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+			cmd := `git commit -m "WIP"`
+			commit := sys.StdCmd(cmd)
+			commit.Dir = pwd
+
+			fmt.Println("COMMAND:", cmd)
+
+			err = commit.Run()
+			sys.Panic(err)
 
 			// push := sys.StdCmd("git push")
 			// push.Dir = pwd
-
-			if err := add.Run(); err == nil {
-				// if err = commit.Run(); err == nil {
-				// 	if err = push.Run(); err == nil {
-				// 		fmt.Println("Done")
-				// 	}
-				// }
-			}
 
 			return err
 		},
