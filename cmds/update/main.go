25b655fe35f31e1699ef65e4b90f486203661021
kronos <kronos@kronoss-MBP.router.asus.com>
2024-08-18 01:23:41 -0500

    "WIP"

--- cmds/update/main.go
+++ cmds/update/main.go
@@ -2,12 +2,23 @@ package update
 
 import (
 	"context"
+	"fmt"
+	"os/exec"
 
 	"jgttech/tools/path"
 	"jgttech/tools/sys"
 
 	"github.com/urfave/cli/v3"
 )
+
+func runFromPath(bin string, wd string) func(cmd string) *exec.Cmd {
+	return func(args string) *exec.Cmd {
+		cmd := sys.StdCmd(fmt.Sprintf(`%s %s`, bin, args))
+		cmd.Dir = wd
+
+		return cmd
+	}
+}
 
 func Command() *cli.Command {
 	return &cli.Command{
@@ -16,21 +27,19 @@ func Command() *cli.Command {
 			pwd, err := path.Join()
 			sys.Panic(err)
 
-			add := sys.StdCmd("git add .")
-			add.Dir = pwd
+			// timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+
+			git := runFromPath("git", pwd)
+
+			add := git(`add .`)
+			commit := git(`commit -m "WIP"`)
+			push := git(`push`)
 
 			err = add.Run()
 			sys.Panic(err)
 
-			// timestamp := strconv.FormatInt(time.Now().Unix(), 10)
-			commit := sys.StdCmd(`git commit -m "WIP"`)
-			commit.Dir = pwd
-
 			err = commit.Run()
 			sys.Panic(err)
-
-			push := sys.StdCmd("git push")
-			push.Dir = pwd
 
 			err = push.Run()
 			sys.Panic(err)
