cdfaea07a96d9073ca0b0f0462563567e35a46c3
kronos <kronos@kronoss-MBP.router.asus.com>
2024-08-18 02:30:24 -0500

    "WIP 2024-08-18 02:30:24.146315 -0500 CDT m=+0.000682067"

--- cmds/version/main.go
+++ cmds/version/main.go
@@ -12,7 +12,7 @@ func Command() *cli.Command {
 	return &cli.Command{
 		Name: "version",
 		Action: func(ctx context.Context, cmd *cli.Command) error {
-			fmt.Printf("%s@%s\n", env.NAME, env.VERSION)
+			fmt.Printf("%s %s\n", env.NAME, env.VERSION)
 			return nil
 		},
 	}
